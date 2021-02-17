6f07fd0539a7c8fe11445791ccde0708187a9282
Mike <tk7294@gmail.com>
2021-02-16 21:58:56 -0800

    fix: refactor holiday population logic

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -3,83 +3,74 @@ import { ResponsiveContainer } from 'recharts';
 import Title from './Title.jsx';
 import Typography from '@material-ui/core/Typography';
 import Calendar from 'react-calendar';
-import { allFederalHolidaysForYear } from './federalHolidays';
+// import fedHolidays from './federalHolidays';
 import 'react-calendar/dist/Calendar.css';
 import './calendarStyles.css';
 import fedHolidays from '@18f/us-federal-holidays';
 
-const highlightHolidays = (pYear) => {
-    let holidaysNew = fedHolidays.allForYear(pYear); // allFederalHolidaysForYear();
-    console.log(holidaysNew);
-
-    let visibleYear = pYear;
-
-    let holidayArray = [];
-
-    // TODO: Refactor this function to use new npm function which takes in a year and returns an array for that year.
-    for (let holiday of holidaysNew) {
-        if (holiday.date.getUTCFullYear() === visibleYear) {
-            holidayArray.push(holiday);
-        }
-        if (holiday.date.getUTCMonth() > visibleYear) {
-            break;
-        }
-    }
-
-    return holidayArray;
-};
-
-const traverseHolidays = (date) => {
-    console.log(date);
-    let nowDate = new Date(date);
-    let holidays = highlightHolidays(nowDate.getUTCFullYear()); // nowDate.getUTCMonth() + 1);
-
-    for (let holiday of holidays) {
-        if (holiday.date.toString() === date.toString()) {
-            return (holiday.name);
-        }
-    }
-}
-
-const isHoliday = (view, date) => {
-    if ( view === 'month' && traverseHolidays(date)){
-        return true;
-    } else{
-        return false;
-    }
-}
-
-let tileClassName = ({ date, view }) => {
-    // Add class to tiles in month view only
-    if (view === 'month') {
-      // Check if a date React-Calendar wants to check is on the list of dates to add class to
-      if (isHoliday(view, date)) {
-        return 'react-calendar--holiday';
-      }
-    }
-}
-
-let tileContent = ({date, view}) => {
-    if(isHoliday(view, date)){
-        return (
-            <Typography>{traverseHolidays(date)}</Typography>
-        );
-    } else {
-        console.log(`no holiday found for ${date}`)
-        return null
-    }
-}
-
 export default function ReactCalendar() {
-    let [dateRange, setDateRange] = useState(new Date());
+    let today = new Date();
+    let [dateRange, setDateRange] = useState(today);
     let [pickUpDate, setPickUPDate] = useState('');
+    let [holidaysForYear, setHolidaysForYear] = useState(fedHolidays.allForYear(today.getUTCFullYear()));
 
-    let calcDateRange = (value) => {
+    let _tileClassName = ({ view, date }) => {
+        // Add class to tiles in month view only
+        if (view === 'month') {
+          // Check if a date React-Calendar wants to check is on the list of dates to add class to
+          if (_isHoliday(view, date)) {
+            return 'react-calendar--holiday';
+          }
+        }
+    }
+
+    let _tileContent = ({view, date}) => {
+        if(_isHoliday(view, date)){
+            return (
+                <Typography>{_traverseHolidaysForNames(date)}</Typography>
+            );
+        } else {
+            return null
+        }
+    }
+
+    // TODO: use this function to create a new array of holidays for new year
+    //  when year in view is no longer current array year
+    
+    // let _populateHolidaysForYear = (year) => {
+    //     let holidayArray = [];
+    
+    //     for (let holiday of holidaysForYear) {
+    //         if(holiday.date.getUTCFullYear() === year){
+    //             holidayArray.push(holiday);
+    //         }
+    //     }
+    
+    //     setHolidaysForYear(holidayArray);
+    // }
+    
+    let _traverseHolidaysForNames = (date) => {
+        for (let holiday of holidaysForYear) {
+            if (holiday.date.toDateString() === date.toDateString()) {
+                return (holiday.name);
+            }
+        }
+    }
+    
+    let _isHoliday = (view, date) => {
+        if ( view === 'month' && _traverseHolidaysForNames(date)){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    let _calcDateRange = (value) => {
         let count = 0;
         
         while (count < 11) {
             var endDate = new Date(value.setDate(value.getDate() + 1));
-            if (endDate.getDay() != 0 && endDate.getDay() != 6 && isHoliday('month', endDate) != true) {
+            if (endDate.getDay() !== 0 && endDate.getDay() !== 6 && _isHoliday('month', endDate) !== true) {
                 count++;
             }
         }
@@ -95,10 +86,10 @@ export default function ReactCalendar() {
                 <Calendar
                     calendarType="US"
                     value={dateRange}
-                    tileContent={tileContent}
-                    tileClassName={tileClassName}
-                    onChange={calcDateRange}
-                    onViewChange={tileContent}
+                    tileContent={_tileContent}
+                    tileClassName={_tileClassName}
+                    onChange={_calcDateRange}
+                    onViewChange={_tileContent}
                 />
             </ResponsiveContainer>
             {pickUpDate ? <Typography>Pick Up Date: {pickUpDate}</Typography> : ''}
